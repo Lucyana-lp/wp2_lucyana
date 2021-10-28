e3f27e6b58406d77af3962562be58a112f7c23b0
lucyana-lp <lucyanalouisap@gmail.com>
2021-10-28 20:31:13 +0700

    jangan Lupa Senyum

---
+++ application/views/beranda.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2> PERKENALAN </H2><br>
+    <p> Nama : Lucyana Louisa Prayitno <br>
+    <p> Nim : 12200492 <br>
+    <p> kelas : 12.3A.15 <br>
+    <p> prodi :Sistem Informasi <br>
+    <p> Alamat : perum villa indah blok m.9 <br>
+    <p> JANGAN LUPA AWALI HARI DENGAN SENYUM <br>
+
+</body>
+</html>
